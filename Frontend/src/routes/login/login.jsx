5155d1eea54be2f5a47e5dfdfdc46396d646c6ff
master <pandyasumit01@gmail.com>
2024-06-29 20:10:22 +0530

    initial commit

--- Frontend/src/routes/login/login.jsx
+++ Frontend/src/routes/login/login.jsx
@@ -8,7 +8,7 @@ function Login() {
   const [error, setError] = useState("");
   const [isLoading, setIsLoading] = useState(false);
 
-  const {updateUser} = useContext(AuthContext)
+  const { updateUser } = useContext(AuthContext);
 
   const navigate = useNavigate();
 
@@ -27,7 +27,7 @@ function Login() {
         password,
       });
 
-      updateUser(res.data)
+      updateUser(res.data);
 
       navigate("/");
     } catch (err) {
@@ -61,7 +61,7 @@ function Login() {
         </form>
       </div>
       <div className="imgContainer">
-        <img src="/bg.png" alt="" />
+        <img src="/bg1.jfif" alt="" />
       </div>
     </div>
   );
--- Frontend/src/routes/register/register.jsx
+++ Frontend/src/routes/register/register.jsx
@@ -48,7 +48,7 @@ function Register() {
         </form>
       </div>
       <div className="imgContainer">
-        <img src="/bg.png" alt="" />
+        <img src="/bg1.jfif" alt="" />
       </div>
     </div>
   );
